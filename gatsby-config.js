2f47795d24145ed7b015ab886d08bf835f0d43dd
listedforlife <clarian@listedproductions.com>
2019-09-25 13:00:25 -0400

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Listed Productions',
+    siteUrl: 'https://listedproductions.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
